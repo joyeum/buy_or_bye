43567ccc71ff9356b6d4b1a636cfb487600ef4ba
joyeum <53428006+joyeum@users.noreply.github.com>
2025-05-30 09:50:51 +0900

    250530 fvm
    build

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.22.2/flutter"
+export "FLUTTER_ROOT=/Users/eumdoyeon/fvm/versions/3.27.1"
 export "FLUTTER_APPLICATION_PATH=/Users/eumdoyeon/Documents/Dev/buy_or_bye"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
